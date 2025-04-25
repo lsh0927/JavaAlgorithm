c4c17da990c101527c84f43bb91ba369fd6c8e30
lsheon0927@naver.com <lsheon0927@naver.com>
2025-04-25 09:22:41 +0900

    0424

--- src/Main.java
+++ src/Main.java
@@ -3,64 +3,81 @@ import java.util.*;
 
 public class Main {
 
-	static class Node{
-		int from;
-		int to;
-		int cost;
-
-		public Node(int from,int to, int cost) {
-			this.from=from;
-			this.to = to;
-			this.cost = cost;
-		}
-	}
-	static int N,M;
-	static int[] parent;
+	static int N,K,W;
+	static int[] D;
+	static List<List<Integer>> list;
+	static int[] degree;
+	static int max;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N=Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		M=Integer.parseInt(st.nextToken());
 
-		parent= new int[N+1];
-		for (int i=1;i<=N;i++){
-			parent[i]=i;
+		int T= Integer.parseInt(br.readLine());
+		for(int i=0;i<T;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N=Integer.parseInt(st.nextToken());
+			K=Integer.parseInt(st.nextToken());
+
+			D= new int[N+1];
+			degree= new int[N+1];
+			st = new StringTokenizer(br.readLine());
+			for (int j=1;j<=N;j++){
+				D[j]=Integer.parseInt(st.nextToken());
+			}
+
+			list= new ArrayList<>();
+			for (int j=0;j<=N;j++){
+				list.add(new ArrayList<>());
+			}
+
+			for (int j=0;j<K;j++) {
+				st = new StringTokenizer(br.readLine());
+				int before= Integer.parseInt(st.nextToken());
+				int after= Integer.parseInt(st.nextToken());
+				degree[after]++;
+				list.get(before).add(after);
+			}
+
+			W= Integer.parseInt(br.readLine());
+			max=-1;
+			cal();
+			System.out.println(max);
 		}
-		PriorityQueue<Node> pq= new PriorityQueue<>(Comparator.comparingInt(a->a.cost));
-
-		for (int i=0;i<M;i++){
-			st= new StringTokenizer(br.readLine());
-			int from= Integer.parseInt(st.nextToken());
-			int to= Integer.parseInt(st.nextToken());
-			int cost= Integer.parseInt(st.nextToken());
-
-			pq.add(new Node(from,to,cost));
-
-		}
-
-		int sum=0;
-		while (!pq.isEmpty()){
-			Node cur= pq.poll();
-			int from= cur.from;
-			int to= cur.to;
-			int cost= cur.cost;
-
-			if(find(from)!=find(to)){
-				union(from,to);
-				sum+=cost;
+	}
+	static void cal() {
+		Queue<int[]> q = new LinkedList<>();
+		for (int i = 1; i <= N; i++) {
+			if (degree[i] == 0) {
+				q.add(new int[]{i, D[i]});
 			}
 		}
 
-		System.out.println(sum);
-	}
-	static int find(int a){
-		if(a==parent[a]) return a;
-		return parent[a]=find(parent[a]);
-	}
-	static void union(int a, int b){
-		a=find(a);
-		b=find(b);
-		if(a!=b) parent[a]=b;
+		//초기화
+		int[] result= new int[N+1];
+		for (int i=1;i<=N;i++){
+			result[i]=D[i];
+		}
+
+
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int n = cur[0];
+			int time = cur[1];
+
+			if (n == W) {
+				max = Math.max(max, time);
+			}
+
+			for (Integer next : list.get(n)) {
+				degree[next]--;
+
+				int newTime=time+D[next];
+				result[next]= Math.max(newTime,result[next]);
+
+				// 진입 차수가 0이 된 경우에만 큐에 추가
+				if (degree[next] == 0) {
+					q.add(new int[]{next, result[next]});
+				}
+			}
+		}
 	}
 }
