12dd6af89b1c2ff7462bd8e3dcf95a29ae8762b5
lsheon0927@naver.com <lsheon0927@naver.com>
2025-04-27 21:13:00 +0900

    0427

---
+++ src/pr/Lv2/PR_택배배달과수거하기.java
@@ -0,0 +1,28 @@
+package pr.Lv2;
+
+public class PR_택배배달과수거하기 {
+	class Solution {
+		public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+			long answer = 0;
+
+			int d=0;
+			int p=0;
+
+			for(int i=n-1;i>=0;i--){
+				if(deliveries[i]>0 || pickups[i]>0){
+					int cnt=0;
+					while(d<deliveries[i] || p<pickups[i]){
+						cnt++;
+						d+=cap;
+						p+=cap;
+					}
+					d-=deliveries[i];
+					p-=pickups[i];
+					answer+=(i+1)*cnt*2;
+				}
+			}
+
+			return answer;
+		}
+	}
+}
