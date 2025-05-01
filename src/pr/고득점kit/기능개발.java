46279eec0d8fec8ca4aab6adea00a452d29f1b6a
lsheon0927@naver.com <lsheon0927@naver.com>
2025-05-02 07:17:03 +0900

    0501

---
+++ src/pr/고득점kit/기능개발.java
@@ -0,0 +1,44 @@
+package pr.고득점kit;
+import java.util.*;
+
+public class 기능개발 {
+	class Solution {
+		public int[] solution(int[] progresses, int[] speeds) {
+			Queue<Integer> queue= new LinkedList<>();
+
+			for(int i=0;i<speeds.length;i++){
+				int cnt=0;
+				int sum=progresses[i];
+				while(sum<100){
+					sum+=speeds[i];
+					cnt++;
+				}
+				queue.add(cnt);
+			}
+
+			int max=0;
+			Map<Integer, Integer> map= new HashMap<>();
+			while(!queue.isEmpty()){
+				int num= queue.poll();
+				if(max>num){
+					num=max;
+				}
+				else{
+					max=num;
+				}
+
+				map.put(num, map.getOrDefault(num,0)+1);
+			}
+
+			List<Integer> list= new ArrayList<>();
+			List<Integer> keyList= new ArrayList<>(map.keySet());
+			Collections.sort(keyList);
+			for(int v: keyList){
+				// System.out.println("key= "+v+" Value= "+map.get(v));
+				list.add(map.get(v));
+			}
+
+			return list.stream().mapToInt(i -> i.intValue()).toArray();
+		}
+	}
+}
