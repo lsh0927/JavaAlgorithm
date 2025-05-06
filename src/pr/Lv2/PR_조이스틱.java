76f38563380bb687f2c37095543af0dbd104aef6
lsheon0927@naver.com <lsheon0927@naver.com>
2025-05-06 11:42:19 +0900

    0506 2회차

--- src/pr/Lv2/PR_조이스틱.java
+++ src/pr/Lv2/PR_조이스틱.java
@@ -1,31 +1,26 @@
 package pr.Lv2;
-import java.util.*;
+class Solution {
+	public int solution(String name) {
+		int move=name.length()-1;
+		int answer=0;
+		for(int i=0;i<name.length();i++){
+			//상하이동
+			answer+= Math.min(name.charAt(i)-'A', 'Z'-name.charAt(i)+1);
 
-public class PR_조이스틱 {
-	class Solution {
-		public int solution(String name) {
-			int move=name.length()-1;
-			int answer=0;
-			for(int i=0;i<name.length();i++){
-				//상하이동
-				answer+= Math.min(name.charAt(i)-'A', 'Z'-name.charAt(i)+1);
+			//좌우 이동
 
-				//죄우 이동
-				int next= i+1;
-				while(next<name.length() && name.charAt(next)=='A'){
-					next++;
-				}
-
-				//오른쪽으로 가기 + 왼쪽으로 가기 (2번의 i번 이동 + len-next만큼 이동)
-				move= Math.min(move, (2*i)+(name.length()-next));
-
-				//BBBBAAAAAAAB: 처음부터 뒷부분을 입력하는게 더 빠를수 있음
-				move = Math.min(move, (name.length() - next) * 2 + i);
-
+			int next= i+1;
+			while(next<name.length() && name.charAt(next)=='A'){
+				next++;
 			}
 
-			answer+=move;
-			return answer;
+			move=Math.min(move, (2*i)+(name.length()-next));
+			move=Math.min(move, (2*(name.length()-next)+ i));
+
+
 		}
+
+		answer+=move;
+		return answer;
 	}
 }
