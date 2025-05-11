d79334bac97b2adfefab56295858b6dc0c3a86a0
lsheon0927@naver.com <lsheon0927@naver.com>
2025-05-11 09:50:44 +0900

    0511 sort

---
+++ src/pr/고득점kit/가장큰수.java
@@ -0,0 +1,34 @@
+package pr.고득점kit;
+import java.util.*;
+
+public class 가장큰수 {
+
+	class Solution {
+		public String solution(int[] numbers) {
+
+			List<String> list= new ArrayList<>();
+
+			for(int number:numbers){
+				list.add(String.valueOf(number));
+			}
+
+			Collections.sort(list, new Comparator<String>(){
+				@Override
+				public int compare(String o1, String o2){
+					String st1= o1+o2;
+					String st2= o2+o1;
+
+					return st2.compareTo(st1);
+				}
+			});
+
+			String answer="";
+			for(String str: list){
+				answer+=str;
+			}
+
+			if(answer.startsWith("0")) answer="0";
+			return answer;
+		}
+	}
+}
